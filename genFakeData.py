9c138dc89ac747e4c2e31e562b6d6ee433f26924
andymann-cs <andylin174@gmail.com>
2025-04-12 21:34:16 +0100

    Update genFakeData.py

--- genFakeData.py
+++ genFakeData.py
@@ -1,15 +1,21 @@
 import random
 from datetime import datetime, timedelta
+from pymongo import MongoClient
 
 #extra users
 #"67e37396300f98be0a832ffb","67e373c2300f98be0a832ffc","67e373d7300f98be0a832ffd","67e373ee300f98be0a832ffe","67e37413300f98be0a832fff","67e37442300f98be0a833000","67e37466300f98be0a833001","67e37488300f98be0a833002","67e374a8300f98be0a833003","67e376a2300f98be0a833004","67e376c9300f98be0a833005"
 
+##CURRENTLY DOES A YEAR WORTH
 class genFakeData:
     def __init__(self):
         self.users = ["67e183c3c468744e4b235553"]
         self.moods = ["Happy", "Sad", "Stressed", "Tired", "Content", "Excited", "Angry", "Proud", "Unsure", "Sick"]
         self.start = datetime(2025,1,1)
         self.end = datetime(2025,12,31)
+
+        self.client = MongoClient("mongodb+srv://second_admin:hL9l8r6liQROX0Up@glowcluster.36bwm.mongodb.net/?retryWrites=true&w=majority&appName=GlowCluster")
+        self.db = self.client["mood_tracker"]
+        self.collection = self.db["moods"]
 
     def genData(self):
         data = []
@@ -65,5 +71,5 @@ class genFakeData:
                                 })
         return data
     
-generator = genFakeData()
-test_data = generator.genData()
+#generator = genFakeData()
+#test_data = generator.collection.insert_many(generator.genData())
