aab79a5ec4dc12ed6f05bde93c63e3377f4368c4
andymann-cs <andylin174@gmail.com>
2025-04-15 22:05:58 +0100

    Update loginsignup.py

--- loginsignup.py
+++ loginsignup.py
@@ -1,35 +1,38 @@
-from PySide6 import QtWidgets, QtGui, QtCore
+#from PySide6 import QtWidgets, QtGui, QtCore
 from better_profanity import profanity
 import re
 import sys
 import hashlib
 import pymongo
 
-client = pymongo.MongoClient(
-    "mongodb+srv://sam_user:9ireiEodVKBb3Owt@glowcluster.36bwm.mongodb.net/?retryWrites=true&w=majority&appName=GlowCluster",
-    tls=True,
-    tlsAllowInvalidCertificates=True
-)
-db = client["mood_tracker"]
-collection = db["accounts"]
+# client = pymongo.MongoClient(
+#     "mongodb+srv://sam_user:9ireiEodVKBb3Owt@glowcluster.36bwm.mongodb.net/?retryWrites=true&w=majority&appName=GlowCluster",
+#     tls=True,
+#     tlsAllowInvalidCertificates=True
+# )
+# db = client["mood_tracker"]
+# collection = db["accounts"]
 
-def encrypt(password): ##PLACEHOLDER FOR ACTUAL ENCRYPTION, BOTH LOGIN AND PASSWORD USE THIS
-    return password
+class loginSignup():
+    def __init__(self, db):
+        self.db = db
 
-def verify_login(username, password_decrypted):
-    login_successful = False  # Custom signal with username
-    password_encrypted = encrypt(password_decrypted)
-    user = collection.find_one({"username": username, "password": password_encrypted})
-    if user:
-        login_successful = True
-    return login_successful
+    def encrypt(self, password): ##PLACEHOLDER FOR ACTUAL ENCRYPTION, BOTH LOGIN AND PASSWORD USE THIS
+        return password
+
+    def verify_login(self, username, password_decrypted):
+        login_successful = False  # Custom signal with username
+        password_encrypted = self.encrypt(password_decrypted)
+
+        self.collection = self.db["accounts"]
+        user = self.collection.find_one({"username": username, "password": password_encrypted})
+        if user:
+            login_successful = True
+        return login_successful
 
 #window.login_successful.connect(lambda username: print(f"Logged in as: {username}")) --What is this?
 
-def SignupLogic(name, sex, age, hobbies, username, password, password_repeat): #Bundles all inputs together atm
-    signup_successful = False  # Define signal
-
-    def contains_embedded_profanity(text):
+    def contains_embedded_profanity(self, text):
         profanity.load_censor_words()
         words = re.findall(r'[a-zA-Z]+', text)  
         for word in words:
@@ -37,27 +40,33 @@ def SignupLogic(name, sex, age, hobbies, username, password, password_repeat): #
                 return True
         return False
 
-    if password != password_repeat:
-        return "passwords do not match"
+
+    def SignupLogic(self, username, password, password_repeat, name, age, sports, hobbies, sex): #Bundles all inputs together atm
+        #signup_successful = False  # Define signal
+
+        if password != password_repeat:
+            return {"error": "passwords do not match"}
+            
+        self.collection = self.db["accounts"]
+        if self.collection.find_one({"username": username}):  # Fixed checking existing user
+            return {"error": "username already exists"}
+            
+        if self.contains_embedded_profanity(username):
+            return {"error": "username must not contain prohibited words"}
+            
+        if username == "" or password == "":
+            return {"error": "please fill out all of the required fields"}
         
-    if collection.find_one({"username": username}):  # Fixed checking existing user
-        return "username already exists"
+        # Save user to database
+        self.collection.insert_one({
+            "username": username,
+            "password": self.encrypt(password),
+            "name": name,
+            "age": age,
+            "exercises": sports,
+            "hobbies": hobbies,
+            "sex": sex, 
+        })
         
-    if contains_embedded_profanity(username):
-        return "username must not contain prohibited words"
-        
-    if username == "" or password == "":
-        return "please fill out all of the required fields"
-    
-    # Save user to database
-    collection.insert_one({
-        "name": name,
-        "sex": sex, 
-        "age": age,
-        "hobbies": hobbies,
-        "username": username,
-        "password": encrypt(password),
-    })
-    
-    signup_successful = True
-    return signup_successful
+
+        return {"message": "Account created Successfully"}
