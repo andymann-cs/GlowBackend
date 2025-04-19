8350089bb46ac8e5cdf9074e9f6d80c47d287c3d
andymann-cs <andylin174@gmail.com>
2025-04-19 13:03:19 +0100

    Update moods.py

--- moods.py
+++ moods.py
@@ -65,7 +65,7 @@ class DB_CRUD():
         self.collection = self.db["accounts"]
         try:
             accountDoc = self.collection.find_one({"username": username})
-            return {"s1": "sd"}
+            return {"s1": username}
             if not accountDoc:
                 return {"error": "User does not exist"}
 
