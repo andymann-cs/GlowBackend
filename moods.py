fcaec2b5396135bc37d1cf259db2a4b5dad81ca0
andymann-cs <andylin174@gmail.com>
2025-04-25 19:17:45 +0100

    Update moods.py

--- moods.py
+++ moods.py
@@ -365,7 +365,7 @@ class DB_CRUD():
 
         self.collection = self.db["moods"]
         moodDoc = self.collection.find({"user_id": user_id, "date": {"gte":start, "lt": end}})
-        return (self.bson_to_dict(doc) for doc in moodDoc)
+        return [self.bson_to_dict(doc) for doc in moodDoc]
 
 
 
