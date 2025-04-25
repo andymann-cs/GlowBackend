ed48ab7dce5e90e1d9e7df82038f1f97b9b8b52c
andymann-cs <andylin174@gmail.com>
2025-04-25 19:31:48 +0100

    Update moods.py

--- moods.py
+++ moods.py
@@ -359,6 +359,7 @@ class DB_CRUD():
     def getMoodEntryByYear(self, username, year):
         user_id = self.getUserID(username)["user_id"]
         
+        year = int(year)
         start = f"{year}-01-01"
         end = f"{year+1}-01-01"
 
